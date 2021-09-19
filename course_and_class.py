c0256a4aff6f72be9ebba30d68dddb645569b398
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-09-19 23:25:45 +0800

    Update course_and_class.py

--- course_and_class.py
+++ course_and_class.py
@@ -30,7 +30,7 @@ class course(db.Model):
         return{"CourseID": self.CourseID, "CourseTitle": self.CourseTitle, "CourseDescription": self.CourseDescription}
 
 @app.route('/courses')
-def get_all():
+def get_all_courses():
     courses = course.query.all()
     if len(courses):
         return jsonify({
@@ -49,57 +49,60 @@ def get_course(CourseID):
     if (course.query.filter_by(CourseID=CourseID).first()):
         return jsonify({
             "code": 200,
-            "message": "Practitioner exists" 
+            "message": "Course exists" 
         }), 201
     else:
         return jsonify({
             "code": 404,
-            "message": "Practitioner doesn't exist"
+            "message": "Course doesn't exist"
         }), 404
 
 
-class class(db.Model):
+class course_class(db.Model):
     __tablename__ = 'class'
 
     CourseID = db.Column(db.Integer, primary_key=True)
     ClassID = db.Column(db.Integer, primary_key=True)
-    # CourseTitle = db.Column(db.String(50), nullable=False)
-    # CourseDescription = db.Column(db.String(65535), nullable=False)
+    TrainerID = db.Column(db.Integer, nullable=False)
+    StartDate = db.Column(db.DateTime, nullable=False)
+    EndDate = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, CourseID, ClassID, CourseDescription):
+    def __init__(self, CourseID, ClassID, TrainerID, StartDate, EndDate):
         self.CourseID = CourseID
         self.ClassID = ClassID
-        # self.CourseDescription = CourseDescription
+        self.TrainerID = TrainerID
+        self.StartDate = StartDate
+        self.EndDate = EndDate
 
     def json(self):
-        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "CourseDescription": self.CourseDescription}
+        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "TrainerID": self.TrainerID, "StartDate": self.StartDate, "EndDate": self.EndDate}
 
 @app.route('/classes')
-def get_all():
-    courses = course.query.all()
-    if len(courses):
+def get_all_classes():
+    classes = course_class.query.all()
+    if len(classes):
         return jsonify({
             "code": 200,
             "data": {
-                "courses": [each_course.json() for each_course in courses]
+                "classes": [each_class.json() for each_class in classes]
             }
         })
     return jsonify({
         "code": 404,
-        "message": "There are no courses."
+        "message": "There are no classes."
     }), 404
 
-@app.route('/classes/<string:CourseID>')
-def get_course(CourseID):
-    if (course.query.filter_by(CourseID=CourseID).first()):
+@app.route('/classes/<string:ClassID>')
+def get_class(ClassID):
+    if (course_class.query.filter_by(ClassID=ClassID).first()):
         return jsonify({
             "code": 200,
-            "message": "Practitioner exists" 
+            "message": "Class exists" 
         }), 201
     else:
         return jsonify({
             "code": 404,
-            "message": "Practitioner doesn't exist"
+            "message": "Class doesn't exist"
         }), 404
 
 if __name__ == '__main__':
