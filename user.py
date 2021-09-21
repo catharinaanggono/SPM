596fcc2e3457f79f2484f988414baebf068aedd0
wellsonah2019 <wellsonah.2019@smu.edu.sg>
2021-09-21 15:00:04 +0800

    added self-enrol backend functionalities

--- user.py
+++ user.py
@@ -2,11 +2,14 @@ from flask import Flask, json, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@localhost:3306/one_stop_lms"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("dbURL") # STOP PUTTING UR PASSWORDS HERE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 86400
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
@@ -17,8 +20,8 @@ class User(db.Model):
     __tablename__ = 'userTable'
 
     UserID = db.Column(db.Integer, primary_key=True)
-    UserName = db.Column(db.String(200), nullable=True)
-    UserType = db.Column(db.String(200), nullable=True)
+    UserName = db.Column(db.String(200), nullable=False)
+    UserType = db.Column(db.String(200), nullable=False)
 
     def __init__ (self, UserID, UserName, UserType):
         self.UserID = UserID
@@ -39,12 +42,40 @@ def get_acct_details(UserID):
             }
         })
     return jsonify({
-        "code": 400,
+        "code": 404,
         "message": "There are no such user"
-    }), 400
+    }), 404
 
 
+class UserCourses(db.Model):
+    __tablename__ = 'learnerCourse'
 
+    LearnerID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, primary_key=True)
+    Status = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, LearnerID, CourseID, Status):
+        self.LearnerID = LearnerID
+        self.CourseID = CourseID
+        self.Status = Status
+
+    def json(self):
+        return {"LearnerID": self.LearnerID, "CourseID": self.CourseID, "Status": self.Status}
+
+@app.route('/user/prereq/<string:UserID>')
+def get_acct_prereqs(UserID):
+    user = UserCourses.query.filter_by(LearnerID = UserID).all()
+    if len(user):
+        return jsonify({
+            "code": 200,
+            "data":{
+                "user_prereqs": [user_prereq.json() for user_prereq in user]
+            }
+        }), 200
+    return jsonify({
+        "code": 404,
+        "message": "User has not completed any courses"
+    }), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
