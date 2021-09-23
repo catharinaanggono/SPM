0dc6556c576e8b93b17384eea1a48e6f0afec189
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-09-23 22:20:13 +0800

    create the get_ongoing_courses function

--- learner_course.py
+++ learner_course.py
@@ -22,7 +22,7 @@ class LearnerCourse(db.Model):
 
     LearnerID = db.Column(db.Integer, primary_key=True)
     CourseID = db.Column(db.Integer, primary_key=True)
-    Status = db.Column(db.String(200), primary_key=True)
+    Status = db.Column(db.String(200), nullable=False)
 
     def __init__(self, LearnerID, CourseID, Status):
         self.LearnerID = LearnerID
@@ -30,13 +30,42 @@ class LearnerCourse(db.Model):
         self.Status = Status
     
     def json(self):
-        return{"LearnerID": self.LearnerID, "CourseID": self.CourseID, "Status": self.Status}
+        if not hasattr(self, 'course_class_id'):
+            self.course_class_id = ''
+        return{"LearnerID": self.LearnerID, "CourseID": self.CourseID, "ClassID": self.course_class_id, "Status": self.Status}
 
-@app.route("/learner_course/<string:UserID>")
-def get_leaner_courses(UserID):
-    Courses = LearnerCourse.query.filter_by(LearnerID = UserID).all()
+class classLearner(db.Model):
+    __tablename__ = 'classLearner'
+
+    CourseID = db.Column(db.Integer, primary_key=True)
+    ClassID = db.Column(db.Integer, primary_key=True)
+    LearnerID = db.Column(db.Integer, primary_key=True)
+    ApplicationStatus = db.Column(db.String(65535),nullable=False)
+
+    def __init__(self, CourseID, ClassID, LearnerID, ApplicationStatus):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.LearnerID = LearnerID
+        self.ApplicationStatus = ApplicationStatus
+    
+    def json(self):
+        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "LearnerID": self.LearnerID, "ApplicationStatus": self.ApplicationStatus}
+
+@app.route("/learner_ongoing_courses/<string:UserID>")
+def get_leaner_ongoing_courses(UserID):
+    Courses = LearnerCourse.query.filter_by(LearnerID = UserID, Status = 'Ongoing').all()
     if len(Courses):
         print(Courses)
+        for a_course in Courses:
+            print(a_course)
+            CourseID = a_course.CourseID
+            print(CourseID)
+            a_class_list = classLearner.query.filter_by(LearnerID = UserID, CourseID = CourseID).first()
+            a_course.course_class_id = a_class_list.ClassID 
+            print(a_class_list)
+            print("LearnerID", a_course.LearnerID)
+            print('EACH COURSE CLASS ID', a_course.course_class_id)
+
         return jsonify(
             {
                 "code": 200,
