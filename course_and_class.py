2499f7e367fcb35f97e57b4449276181e7b1a085
wellsonah2019 <wellsonah.2019@smu.edu.sg>
2021-09-26 00:20:11 +0800

    added checking for registration date, remaining class size, prerequisites, and if user has enrolled

--- course_and_class.py
+++ course_and_class.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from flask_cors import CORS
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -60,20 +61,25 @@ class course_class(db.Model):
     ClassID = db.Column(db.Integer, primary_key=True)
     StartDate = db.Column(db.DateTime, nullable=False)
     EndDate = db.Column(db.DateTime, nullable=False)
+    ClassSize = db.Column(db.Integer, nullable = False)
     RegistrationStartDate = db.Column(db.DateTime, nullable=False)
     RegistrationEndDate = db.Column(db.DateTime, nullable=False)
 
 
-    def __init__(self, CourseID, ClassID, StartDate, EndDate, RegistrationStartDate, RegistrationEndDate):
+    def __init__(self, CourseID, ClassID, StartDate, EndDate, ClassSize, RegistrationStartDate, RegistrationEndDate):
         self.CourseID = CourseID
         self.ClassID = ClassID
         self.StartDate = StartDate
         self.EndDate = EndDate
-        self.RegistrationEndDate = RegistrationEndDate
+        self.ClassSize = ClassSize
         self.RegistrationStartDate = RegistrationStartDate
+        self.RegistrationEndDate = RegistrationEndDate
+        
 
     def json(self):
-        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "StartDate": self.StartDate, "EndDate": self.EndDate, "RegistrationStartDate": self.RegistrationStartDate, "RegistrationEndDate": self.RegistrationEndDate}
+        if not hasattr(self, 'GreyOut'):
+            self.GreyOut = False
+        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "StartDate": self.StartDate, "EndDate": self.EndDate, "ClassSize": self.ClassSize, "RegistrationStartDate": self.RegistrationStartDate, "RegistrationEndDate": self.RegistrationEndDate, "GreyOut": self.GreyOut}
 
 @app.route('/classes')
 def get_all_classes():
@@ -103,13 +109,7 @@ def get_class(ClassID):
             "message": "Class doesn't exist"
         }), 404
 
-# @app.route('/classespercourse/<string:CourseID>')
-# def get_class_per_course(CourseID):
-#     classes = course_class.query.filter_by(CourseID = CourseID)
-#     return jsonify({
-#         "code": 200,
-#         "data": [each_class.json() for each_class in classes]
-#     })
+
 class course_prereq(db.Model):
     __tablename__ = 'coursePrereq'
 
@@ -138,17 +138,86 @@ def get_prereq():
         "message": "There are no prerequisites"
     }), 404
 
-@app.route('/courses')
-def get_all_courses():
-    courses = course.query.all()
-    for a_course in courses:
-        CourseID = a_course.CourseID
-        classes = course_class.query.filter_by(CourseID = CourseID)
-        class_list = [a_class.json() for a_class in classes]
-        a_course.classList = class_list
+class ClassTaken(db.Model):
+    __tablename__ = 'classLearner'
+
+    # CourseID INT NOT NULL,
+    # ClassID INT NOT NULL,
+    # LearnerID INT NOT NULL,
+    # ApplicationStatus TEXT NOT NULL, -- applied, enrolled, rejected
+
+    CourseID = db.Column(db.Integer, primary_key = True)
+    ClassID = db.Column(db.Integer, primary_key = True)
+    LearnerID = db.Column(db.Integer, primary_key = True)
+    ApplicationStatus = db.Column(db.String(100), nullable = False)
+
+    def __init__(self, CourseID, ClassID, LearnerID, ApplicationStatus):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.LearnerID = LearnerID
+        self.ApplicationStatus = ApplicationStatus
+
+    def json(self):
+        
+        return {"CourseID": self.CourseID, "ClassID": self.ClassID, "LearnerID": self.LearnerID}
+
+# CHECK REMAINING CLASS SLOTS -- DONE
+
+# IF REJECTED, ALLOW APPLICATION AGAIN
+
+# Checking for completed prereq
+
+class LearnerCourse(db.Model):
+    __tablename__ = 'LearnerCourse'
+
+    LearnerID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, primary_key=True)
+    Status = db.Column(db.String(200), primary_key=True)
+
+    def __init__(self, LearnerID, CourseID, Status):
+        self.LearnerID = LearnerID
+        self.CourseID = CourseID
+        self.Status = Status
     
-    print(a_course.classList)
-    if len(courses):
+    def json(self):
+        return{"LearnerID": self.LearnerID, "CourseID": self.CourseID, "Status": self.Status}
+
+
+@app.route('/courses-all/<string:UserID>')
+def get_all_courses(UserID):
+    enrolled_courses = []
+    takenClasses = ClassTaken.query.filter_by(LearnerID = UserID)
+
+    prereq_courses = []
+    takenPrereq = LearnerCourse.query.filter_by(LearnerID = UserID)
+    for prereq in takenPrereq:
+        prereq_courses.append(prereq.CourseID)
+    for a_class in takenClasses:
+        if a_class.ApplicationStatus == 'enrolled': # check if user already enrolled
+            enrolled_courses.append(a_class.CourseID)
+    courses = course.query.filter(course.CourseID not in enrolled_courses)
+    x = 1
+    for a_course in courses:
+        if a_course.CourseID not in enrolled_courses:
+            CourseID = a_course.CourseID
+            course_prereqs = course_prereq.query.filter_by(CourseID = CourseID)
+            for a_prereq in course_prereqs:
+                if a_prereq.PrereqID not in prereq_courses:
+                    x = 0
+            now = datetime.date(datetime.today())
+            shown_classes = []
+            classes = course_class.query.filter(CourseID == CourseID, now < course_class.RegistrationEndDate, now > course_class.RegistrationStartDate) # check registration date
+            for a_class in classes:
+                if x == 0:
+                    a_class.GreyOut = True
+                currentlyEnrolled = ClassTaken.query.filter(CourseID == CourseID, a_class.ClassID == a_class.ClassID, ClassTaken.ApplicationStatus == "enrolled") # check remaining class sizes
+                totalEnrolled = currentlyEnrolled.count()
+                if totalEnrolled < a_class.ClassSize:
+                    shown_classes.append(a_class)
+            class_list = [a_class.json() for a_class in shown_classes]
+            a_course.classList = class_list
+        x = 1
+    if courses.count():
         return jsonify({
             "code": 200,
             "data": {
