a0ad1346424450abb99f3d13606d6bbe806fea7a
jtedja98 <jtedja.2019@scis.smu.edu.sg>
2021-10-05 19:32:21 +0800

    link UI page with create course function, need to add validation

--- course_hr.py
+++ course_hr.py
@@ -30,12 +30,11 @@ class Course(db.Model):
         self.CourseTitle = CourseTitle
         self.CourseDescription = CourseDescription
         self.Badge = Badge
-        # self.prereqList = []
 
-    # def json(self):
-    #     if not hasattr(self, 'prereqList'):
-    #         self.prereqList = []
-    #     return{"CourseID": self.CourseID, "CourseTitle": self.CourseTitle, "CourseDescription": self.CourseDescription, "Badge": self.Badge, "prereqList": self.prereqList}
+    def json(self):
+        if not hasattr(self, 'prereqList'):
+            self.prereqList = []
+        return{"CourseID": self.CourseID, "CourseTitle": self.CourseTitle, "CourseDescription": self.CourseDescription, "Badge": self.Badge}
 
 class CoursePrereq(db.Model):
     __tablename__ = 'coursePrereq'
@@ -78,18 +77,18 @@ class Class(db.Model):
 
 
 
-@app.route('/courses/<string:CourseID>')
-def get_course(CourseID):
-    if (Course.query.filter_by(CourseID=CourseID).first()):
-        return jsonify({
-            "code": 200,
-            "message": "Course exists" 
-        }), 201
-    else:
-        return jsonify({
-            "code": 404,
-            "message": "Course doesn't exist"
-        }), 404
+# @app.route('/courses/<string:CourseID>')
+# def get_course(CourseID):
+#     if (Course.query.filter_by(CourseID=CourseID).first()):
+#         return jsonify({
+#             "code": 200,
+#             "message": "Course exists" 
+#         }), 201
+#     else:
+#         return jsonify({
+#             "code": 404,
+#             "message": "Course doesn't exist"
+#         }), 404
 
 @app.route("/create_course", methods=["POST"])
 def create_course():
@@ -104,66 +103,57 @@ def create_course():
     db.session.add(course)
     db.session.commit()
 
-
-    for prereq in prereqList:
-        coursePrereq = CoursePrereq(course.CourseID, prereq)
-        print(coursePrereq)
-        db.session.add(coursePrereq)
+    if prereqList:
+        for prereq in prereqList:
+            coursePrereq = CoursePrereq(course.CourseID, prereq)
+            print(coursePrereq)
+            db.session.add(coursePrereq)
 
     db.session.commit()
      
     return jsonify(
         {
-            "code": 201,
+            "code": 200,
             "message": "Course is successfully created"
         }
-    ), 201        
+    ), 200
+
+@app.route('/courses')
+def get_all_courses():
+    courses = Course.query.all()
+        
+    if len(courses):
+        return jsonify({
+            "code": 200,
+            "data": {
+                "courses": [course.json() for course in courses]
+            }
+        }), 200
+    return jsonify({
+        "code": 404,
+        "message": "There are no courses."
+    }), 404
 
 
-@app.route("/create_class", methods=["POST"])
-def create_class():
-    data = request.get_json()
-    cl = Class(**data)
-    print(data)
-    ClassSize = data['ClassSize']
-    StartDate = data['StartDate']
-    EndDate = data['EndDate']
+# @app.route("/create_class", methods=["POST"])
+# def create_class():
+#     data = request.get_json()
+#     cl = Class(**data)
+#     print(data)
+#     ClassSize = data['ClassSize']
+#     StartDate = data['StartDate']
+#     EndDate = data['EndDate']
 
-    db.session.add(cl)
-    db.session.commit()
+#     db.session.add(cl)
+#     db.session.commit()
      
-    return jsonify(
-        {
-            "code": 201,
-            "message": "Class is successfully created"
-        }
-    ), 201       
+#     return jsonify(
+#         {
+#             "code": 201,
+#             "message": "Class is successfully created"
+#         }
+#     ), 201       
 
-
-    # if (course.query.filter_by(CourseTitle=CourseTitle).first()):
-    #     return jsonify(
-    #         {
-    #             "code": 400,
-    #             "message": "Course already exists."
-    #         }
-    #     ), 400
-    # try:
-    #     db.session.add(c)
-    #     db.session.commit()
-    # except:
-    #    return jsonify(
-    #        {
-    #             "code": 500,
-    #             "message": "An error occurred in creating a course."
-    #        }
-    #    ), 500
- 
-    # return jsonify(
-    #     {
-    #         "code": 201,
-    #         "data": c.json()
-    #     }
-    # ), 201
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5005, debug=True)
