ef717781832e4fc6ea6c122e70bf852f32f1f360
Rivaldi0097 <72537323+Rivaldi0097@users.noreply.github.com>
2021-10-28 21:48:46 +0800

    Update integration_test_riv.py

--- integration_test_riv.py
+++ integration_test_riv.py
@@ -23,17 +23,17 @@ class TestApp(flask_testing.TestCase):
         course2 = course(4, "WAD2", "Teach about programming", "python_badge.png")
         prereq1 = prereq(4, 3)
         class1 = Class(3, 1, datetime.strptime('31-12-2021 00:00:00.000000', '%d-%m-%Y %H:%M:%S.%f'), datetime.strptime('30-06-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), 20, datetime.strptime('01-10-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), datetime.strptime('31-12-2021 00:00:00', '%d-%m-%Y %H:%M:%S'))
+        class2 = Class(4, 1, datetime.strptime('31-12-2021 00:00:00.000000', '%d-%m-%Y %H:%M:%S.%f'), datetime.strptime('30-06-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), 20, datetime.strptime('01-10-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), datetime.strptime('31-12-2021 00:00:00', '%d-%m-%Y %H:%M:%S'))
         learner1 = User(1, "Jhonny", "Senior Engineer")
-        classLearner1 = classLearner(3, 1, 1, "completed")
         
-
 
         db.session.add(learner1)
         db.session.add(course1)
         db.session.add(course2)
         db.session.add(class1)
+        db.session.add(class2)
         db.session.add(prereq1)
-        db.session.add(classLearner1)
+        
         db.session.commit()
 
     def tearDown(self):
@@ -43,10 +43,10 @@ class TestApp(flask_testing.TestCase):
 class TestRetrieveLearner(TestApp):
     def test_retrieve_learner(self):
 
-        class2 = Class(4, 1, datetime.strptime('31-12-2021 00:00:00.000000', '%d-%m-%Y %H:%M:%S.%f'), datetime.strptime('30-06-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), 20, datetime.strptime('01-10-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), datetime.strptime('31-12-2021 00:00:00', '%d-%m-%Y %H:%M:%S'))
+        classLearner1 = classLearner(3, 1, 1, "completed")
         classLearner2 = classLearner(4, 1, 1, "ongoing")
 
-        db.session.add(class2)
+        db.session.add(classLearner1)
         db.session.add(classLearner2)
 
         request_body = {
@@ -69,8 +69,11 @@ class TestRetrieveLearner(TestApp):
     
     def test_prereq(self):
         
+        classLearner3 = classLearner(3, 1, 1, "ongoing")
+        classLearner4 = classLearner(4, 1, 1, "self_enrolled")
 
-
+        db.session.add(classLearner3)
+        db.session.add(classLearner4)
 
 
         request_body = {
@@ -85,11 +88,34 @@ class TestRetrieveLearner(TestApp):
         self.assertEqual(response.json,{
             "code": 200,
             "data":{
-                "user":[{'UserID': 1, 
-                        'UserName': 'Jhonny', 
-                        'UserType': 'Senior Engineer'}]
+                "user":[]
             }
         })
+    
+    def test_no_prereq(self):
+        
+        course3 = course(1, "SPM", "Teach about programming", "python_badge.png")
+        class3 = Class(1, 1, datetime.strptime('31-12-2021 00:00:00.000000', '%d-%m-%Y %H:%M:%S.%f'), datetime.strptime('30-06-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), 20, datetime.strptime('01-10-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), datetime.strptime('31-12-2021 00:00:00', '%d-%m-%Y %H:%M:%S'))
+        
+        db.session.add(course3)
+
+
+        request_body = {
+            'CourseID': 4
+        }
+
+
+        response = self.client.post("/HrAssign",
+                                    data=json.dumps(request_body),
+                                    content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 200,
+            "data":{
+                "user":[]
+            }
+        })
+
 
 
 if __name__ == '__main__':
