d1042d443a4001728b4779d9cc83de978de3664d
catharinaanggono <catharinaa.2019@sis.smu.edu.sg>
2021-11-02 01:19:36 +0800

    tdd done

--- course_and_class.py
+++ course_and_class.py
@@ -243,8 +243,6 @@ class Section(db.Model):
             self.SectionMaterialList = []
         return {"CourseID": self.CourseID, "ClassID": self.ClassID, "SectionID": self.SectionID,"SectionName": self.SectionName, "SectionMaterialList": self.SectionMaterialList}
 
-
-
 class StudentQuizResult(db.Model):
     __tablename__ = 'studentQuizResult'
 
@@ -668,6 +666,418 @@ def view_material(CourseID, ClassID, SectionID, url):
         return render_template('view-file.html', CourseID=CourseID, ClassID=ClassID, SectionID=SectionID, url=url)
     else:
         abort(403)
+
+        # ungraded quiz
+class Quiz(db.Model):
+    __tablename__ = 'quiz'
+
+    QuizID = db.Column(db.Integer, primary_key = True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    ClassID = db.Column(db.Integer, nullable=False)
+    SectionID = db.Column(db.Integer, nullable=False)
+    QuizTitle = db.Column(db.String(50), nullable=False)
+    QuizTimer = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, CourseID, ClassID, SectionID, QuizTitle, QuizTimer):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.SectionID = SectionID
+        self.QuizTitle = QuizTitle
+        self.QuizTimer = QuizTimer
+
+    def json(self):
+        return{"QuizID": self.QuizID, "CourseID": self.CourseID, "ClassID": self.ClassID, "SectionID": self.SectionID, "QuizTitle": self.QuizTitle, "QuizTimer": self.QuizTimer}
+
+class Question(db.Model):
+    __tablename__ = 'question'
+
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    ClassID = db.Column(db.Integer, nullable=False)
+    SectionID = db.Column(db.Integer, nullable=False)
+    QuizID = db.Column(db.Integer, nullable=False)
+    QuestionContent = db.Column(db.Text, nullable=False)
+
+
+    def __init__(self, CourseID, ClassID, SectionID, QuizID, QuestionContent):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.SectionID = SectionID
+        self.QuizID = QuizID
+        self.QuestionContent = QuestionContent
+    
+    def json(self):
+        return{"QuestionID": self.QuestionID, "CourseID": self.CourseID, "ClassID": self.ClassID, "SectionID": self.SectionID, "QuizID": self.QuizID, "QuestionContent": self.QuestionContent}
+
+class QuestionAnswer(db.Model):
+    __tablename__ = 'questionAnswer'
+
+    AnswerID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    ClassID = db.Column(db.Integer, nullable=False)
+    SectionID = db.Column(db.Integer, nullable=False)
+    QuizID = db.Column(db.Integer, nullable=False)
+    QuestionID = db.Column(db.Integer, nullable=False)
+    AnswerContent = db.Column(db.Text, nullable=False)
+    Correct = db.Column(db.Boolean, nullable=False)
+
+
+    def __init__(self, CourseID, ClassID, SectionID, QuizID, QuestionID, AnswerContent, Correct):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.SectionID = SectionID
+        self.QuizID = QuizID
+        self.QuestionID = QuestionID
+        self.AnswerContent = AnswerContent
+        self.Correct = Correct
+    
+    def json(self):
+        return{"AnswerID": self.AnswerID, "CourseID": self.CourseID, "ClassID": self.ClassID, "SectionID": self.SectionID, "QuizID": self.QuizID, "QuestionID": self.QuestionID, "AnswerContent": self.AnswerContent, "Correct": self.Correct}
+
+class StudentAnswer(db.Model):
+    __tablename__ = 'studentAnswer'
+
+    CourseID = db.Column(db.Integer, primary_key=True)
+    ClassID = db.Column(db.Integer, primary_key=True)
+    SectionID = db.Column(db.Integer, primary_key=True)
+    QuizID = db.Column(db.Integer, primary_key=True)
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    LearnerID = db.Column(db.Integer, primary_key=True)
+    AnswerID = db.Column(db.Integer, nullable=False)
+    AttemptID = db.Column(db.Integer, primary_key=True)
+
+
+    def __init__(self, CourseID, ClassID, SectionID, QuizID, QuestionID, LearnerID, AnswerID, AttemptID):
+        self.CourseID = CourseID
+        self.ClassID = ClassID
+        self.SectionID = SectionID
+        self.QuizID = QuizID
+        self.QuestionID = QuestionID
+        self.LearnerID = LearnerID
+        self.AnswerID = AnswerID
+        self.AttemptID = AttemptID
+    
+    def json(self):
+        return{"CourseID": self.CourseID, "ClassID": self.ClassID, "SectionID": self.SectionID, "QuizID": self.QuizID, "QuestionID": self.QuestionID, "LearnerID": self.LearnerID, "AnswerID": self.AnswerID, "AttemptID": self.AttemptID}
+
+# graded quiz
+class FinalQuiz(db.Model):
+    __tablename__ = 'finalQuiz'
+
+    QuizID = db.Column(db.Integer, primary_key = True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    QuizTitle = db.Column(db.String(50), nullable=False)
+    QuizTimer = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, CourseID, QuizTitle, QuizTimer):
+        self.CourseID = CourseID
+        self.QuizTitle = QuizTitle
+        self.QuizTimer = QuizTimer
+
+    def json(self):
+        return{"QuizID": self.QuizID, "CourseID": self.CourseID, "QuizTitle": self.QuizTitle, "QuizTimer": self.QuizTimer}
+
+class FinalQuizQuestion(db.Model):
+    __tablename__ = 'finalQuizQuestion'
+
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    QuizID = db.Column(db.Integer, nullable=False)
+    QuestionContent = db.Column(db.Text, nullable=False)
+
+
+    def __init__(self, CourseID, QuizID, QuestionContent):
+        self.CourseID = CourseID
+        self.QuizID = QuizID
+        self.QuestionContent = QuestionContent
+    
+    def json(self):
+        return{"QuestionID": self.QuestionID, "CourseID": self.CourseID, "QuizID": self.QuizID, "QuestionContent": self.QuestionContent}
+
+class FinalQuizQuestionAnswer(db.Model):
+    __tablename__ = 'finalQuizQuestionAnswer'
+
+    AnswerID = db.Column(db.Integer, primary_key=True)
+    CourseID = db.Column(db.Integer, nullable=False)
+    QuizID = db.Column(db.Integer, nullable=False)
+    QuestionID = db.Column(db.Integer, nullable=False)
+    AnswerContent = db.Column(db.Text, nullable=False)
+    Correct = db.Column(db.Boolean, nullable=False)
+
+
+    def __init__(self, CourseID, QuizID, QuestionID, AnswerContent, Correct):
+        self.CourseID = CourseID
+        self.QuizID = QuizID
+        self.QuestionID = QuestionID
+        self.AnswerContent = AnswerContent
+        self.Correct = Correct
+    
+    def json(self):
+        return{"AnswerID": self.AnswerID, "CourseID": self.CourseID, "QuizID": self.QuizID, "QuestionID": self.QuestionID, "AnswerContent": self.AnswerContent, "Correct": self.Correct}
+
+# for ungraded quiz
+@app.route('/create_quiz', methods=["POST"])
+def create_quiz():
+
+    data = request.get_json()
+    print(data)
+
+    quiz = Quiz(**data)
+
+    try:
+        db.session.add(quiz)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the quiz."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Quiz is successfully created",
+            "data": {
+                "QuizID": quiz.QuizID
+            }
+        }
+    ), 201     
+
+@app.route('/create_question', methods=["POST"])
+def create_question():
+
+    data = request.get_json()
+    print(data)
+
+    question = Question(**data)
+
+    try:
+        db.session.add(question)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the question."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Question is successfully created",
+            "data": {
+                "QuestionID": question.QuestionID
+            }
+        }
+    ), 201     
+
+@app.route('/create_answer', methods=["POST"])
+def create_answer():
+
+    data = request.get_json()
+    print(data)
+
+    answer = QuestionAnswer(**data)
+
+    try:
+        db.session.add(answer)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the answer."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Answer is successfully created"
+        }
+    ), 201     
+
+# for graded quiz
+@app.route('/create_graded_quiz', methods=["POST"])
+def create_graded_quiz():
+
+    data = request.get_json()
+    print(data)
+
+    final_quiz = FinalQuiz(**data)
+
+    try:
+        db.session.add(final_quiz)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the final quiz."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Final quiz is successfully created",
+            "data": {
+                "QuizID": final_quiz.QuizID
+            }
+        }
+    ), 201     
+
+@app.route('/create_graded_question', methods=["POST"])
+def create_graded_question():
+
+    data = request.get_json()
+    print(data)
+
+    final_quiz_question = FinalQuizQuestion(**data)
+
+    try:
+        db.session.add(final_quiz_question)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the question."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Question is successfully created",
+            "data": {
+                "QuestionID": final_quiz_question.QuestionID
+            }
+        }
+    ), 201    
+
+@app.route('/create_graded_answer', methods=["POST"])
+def create_graded_answer():
+
+    data = request.get_json()
+    print(data)
+
+    final_quiz_answer = FinalQuizQuestionAnswer(**data)
+
+    try:
+        db.session.add(final_quiz_answer)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the answer."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Answer is successfully created"
+        }
+    ), 201     
+
+# get ungraded quiz
+@app.route('/get_quiz/<string:QuizID>')
+def get_quiz(QuizID):
+    get_quiz_content = Quiz.query.filter(Quiz.QuizID == QuizID).first()
+    get_quiz_questions = Question.query.filter(Question.QuizID == QuizID).all()
+    get_quiz_answers = QuestionAnswer.query.filter(QuestionAnswer.QuizID == QuizID).all()
+    if get_quiz_content:
+        return jsonify({
+            "code": 200,
+            "data":{
+                "Quiz": get_quiz_content.json(),
+                "Questions":
+                    [question.json() for question in get_quiz_questions],
+                "Answers":
+                    [answer.json() for answer in get_quiz_answers]                
+            }
+        })
+    return jsonify({
+        "code":400,
+        "message":"There is  no quiz"
+    }), 404
+
+#post student answers for ungraded quiz
+@app.route('/post_ans', methods=["POST"])
+def post_ans():
+    data = request.get_json()
+    print(data)
+
+    stu_answer = StudentAnswer(**data)
+
+    try:
+        db.session.add(stu_answer)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the answer."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Answer is successfully sent"
+        }
+    ), 201     
+
+#post student result for ungraded quiz
+@app.route('/post_result', methods=["POST"])
+def post_result():
+    data = request.get_json()
+    print(data)
+
+    stu_result = StudentQuizResult(**data)
+
+    try:
+        db.session.add(stu_result)    
+        db.session.commit()
+    
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating student's result."
+            }
+        ), 500
+
+
+    return jsonify(
+        {
+            "code": 201,
+            "message": "Result is successfully created",
+            "data": {
+                "AttemptID": stu_result.AttemptID
+            }
+        }
+    ), 201     
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
 
---
+++ integration_tests_catharina.py
@@ -0,0 +1,207 @@
+import unittest
+import flask_testing
+import json
+from course_and_class import app, db, User, course, course_class, Section, Quiz, Question, QuestionAnswer, FinalQuiz, FinalQuizQuestion, FinalQuizQuestionAnswer
+from datetime import datetime
+
+class TestApp(flask_testing.TestCase):
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+    app.config['TESTING'] = True
+    app.config.update({
+    'SQLALCHEMY_POOL_SIZE': None,
+    'SQLALCHEMY_POOL_TIMEOUT': None
+})
+
+    def create_app(self):
+        return app
+
+    def setUp(self):
+        db.create_all()
+        user_1 = User(1, 'Brenda', 'Senior Engineer')
+        course_1 = course(1, 'Test Course 1', 'Test Course 1 Description', 'Test Course 1 Badge')
+        class_1 = course_class(1, 1, datetime.strptime('31-12-2021 00:00:00.000000', '%d-%m-%Y %H:%M:%S.%f'), datetime.strptime('30-06-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), 20, datetime.strptime('01-10-2021 00:00:00', '%d-%m-%Y %H:%M:%S'), datetime.strptime('31-12-2021 00:00:00', '%d-%m-%Y %H:%M:%S'))
+        section_1 = Section(1, 1, "Test Section 1")
+        db.session.add(user_1)
+        db.session.add(course_1)
+        db.session.add(class_1)
+        db.session.add(section_1)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+class TestCreateUngradedQuiz(TestApp):    
+    def test_create_quiz(self):
+        request_body = {
+            "CourseID": 1,
+            "ClassID": 1,
+            "SectionID": 1,
+            "QuizTitle": "Test Quiz 1",
+            "QuizTimer" : 60
+        }
+
+        response = self.client.post("/create_quiz",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Quiz is successfully created",
+            "data": {
+                "QuizID": 1,
+            }
+        })
+
+    def test_create_question(self):
+        request_body = {
+            "CourseID": 1,
+            "ClassID": 1,
+            "SectionID": 1,
+            "QuizID": 1,
+            "QuestionContent" : "Test Question 1"
+        }
+
+        response = self.client.post("/create_question",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Question is successfully created",
+            "data": {
+                "QuestionID": 1,
+            }
+        })
+
+    def test_create_answer(self):
+        request_body = {
+            "CourseID": 1,
+            "ClassID": 1,
+            "SectionID": 1,
+            "QuizID": 1,
+            "QuestionID": 1,
+            "AnswerContent": "Test Answer 1",
+            "Correct": 1
+        }
+
+        response = self.client.post("/create_answer",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Answer is successfully created",
+        })
+
+class TestGetUngradedQuiz(TestApp):
+    maxDiff = None
+    def test_retrieve_(self):
+        quiz1 = Quiz(1, 1, 1, 'Test Quiz 1', 60)
+        question1 = Question(1, 1, 1, 1, 'Test Question 1')
+        answer1 = QuestionAnswer(1, 1, 1, 1, 1, 'Test Answer 1', 1)
+
+        db.session.add(quiz1)
+        db.session.add(question1)
+        db.session.add(answer1)
+
+        response = self.client.get("/get_quiz/1")    
+        self.assertEqual(response.json, 
+        {
+            "code": 200,
+            "data": {
+                "Answers": [
+                    {
+                        "AnswerContent": "Test Answer 1",
+                        "AnswerID": 1,
+                        "ClassID": 1,
+                        "Correct": True,
+                        "CourseID": 1,
+                        "QuestionID": 1,
+                        "QuizID": 1,
+                        "SectionID": 1
+                    }
+                ],
+                "Questions": [
+                    {
+                        "ClassID": 1,
+                        "CourseID": 1,
+                        "QuestionContent": "Test Question 1",
+                        "QuestionID": 1,
+                        "QuizID": 1,
+                        "SectionID": 1
+                    }
+                ],
+                "Quiz": {
+                    "ClassID": 1,
+                    "CourseID": 1,
+                    "QuizID": 1,
+                    "QuizTimer": 60,
+                    "QuizTitle": "Test Quiz 1",
+                    "SectionID": 1
+                }
+            }
+        })
+
+class TestCreateFinalQuiz(TestApp):    
+    def test_create_final_quiz(self):
+        request_body = {
+            "CourseID": 1,
+            "QuizTitle": "Test Final Quiz 1",
+            "QuizTimer" : 60
+        }
+
+        response = self.client.post("/create_graded_quiz",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Final quiz is successfully created",
+            "data": {
+                "QuizID": 1,
+            }
+        })
+
+    def test_create_final_quiz_question(self):
+        request_body = {
+            "CourseID": 1,
+            "QuizID": 1,
+            "QuestionContent" : "Test Question 1"
+        }
+
+        response = self.client.post("/create_graded_question",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Question is successfully created",
+            "data": {
+                "QuestionID": 1,
+            }
+        })
+
+    def test_create_final_quiz_answer(self):
+        request_body = {
+            "CourseID": 1,
+            "QuizID": 1,
+            "QuestionID": 1,
+            "AnswerContent": "Test Answer 1",
+            "Correct": 1
+        }
+
+        response = self.client.post("/create_graded_answer",
+                            data=json.dumps(request_body),
+                            content_type='application/json')
+
+        self.assertEqual(response.json,{
+            "code": 201,
+            "message": "Answer is successfully created",
+        })
+
+
+if __name__ == '__main__':
+    unittest.main()
